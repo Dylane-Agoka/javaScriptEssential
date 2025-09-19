3c769fa6712268001b9fe3841dbf9ef8a0fbe4f7
Dylane AGOKA <dylane.agoka@inetconsulting.cm>
2025-09-19 11:42:39 +0100

    colorful Memory Match Game: adding checkMartch, startGame and startGameTimer

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -29,3 +29,66 @@ function shuffle(array) {
   }
   return array;
 }
+
+function handleCardClick(event) {
+  const card = event.target;
+  if (!card.classList.contains('card') || card.classList.contains('matched')) {
+    return;
+  }
+  card.textContent = card.dataset.color;
+  card.style.backgroundColor = card.dataset.color;
+  selectedCards.push(card);
+  if (selectedCards.length === 2) {
+    setTimeout(checkMatch,500);
+  }
+}
+
+function checkMatch() {
+  const [card1, card2] = selectedCards;
+  if (card1.dataset.color === card2.dataset.color) {
+    card1.classList.add('matched');
+    card2.classList.add('matched');
+    score += 2;
+    scoreElement.textContent = `Score: ${score}`;
+  } else {
+    card1.textContent = '?';
+    card2.textContent = '?';
+    card1.style.backgroundColor = '#ddd';
+    card2.style.backgroundColor = '#ddd';
+  }
+  selectedCards = [];
+}
+
+function startGame() {
+  let timeLeft = 30;
+  startbtn.disabled = true;
+  startbtn.style.backgroundColor = '#3498db';
+  startbtn.style.color = 'gray';
+  score = 0; // Reset score to zero
+  scoreElement.textContent = `Score: ${score}`;
+  startGameTimer(timeLeft);
+  cards = shuffle(colors.concat(colors));
+  selectedCards = [];
+  gameContainer.innerHTML = '';
+  generateCards();
+  gameContainer.addEventListener('click', handleCardClick);
+}
+
+function startGameTimer(timeLeft) {
+  timerElement.textContent = `Time Left: ${timeLeft}`;
+  gameInterval = setInterval(() => {
+    timeLeft--;
+    timerElement.textContent = `Time Left: ${timeLeft}`;
+
+    if (timeLeft === 0) {
+      clearInterval(gameInterval);
+      let timeLeft = 30;
+      alert('Game over!');
+      startbtn.disabled = false;
+      startbtn.style.backgroundColor = 'white';
+      startbtn.style.color = '#3498db';
+    }
+  }, 1000);
+}
+
+startbtn.addEventListener('click', startGame);
